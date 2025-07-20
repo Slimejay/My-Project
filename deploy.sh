dcdef50b2a3dc375359b9af5b85f8c6a32fc6351
Efeturi Ebiai <ebiaiefeturi@gmail.com>
2025-07-20 23:37:19 +0100

    Test GitHub workflow trigger

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,4 @@ sudo yum install -y nodejs
 echo "📦 Ensuring PM2 is globally available..."
 sudo npm install -g pm2
 
-cd /home/ec2-user/app  # Use ec2-user if Amazon Linux
 
-npm install
-
-pm2 describe myapp > /dev/null
-if [ $? -eq 0 ]; then
-  echo "Restarting app with PM2..."
-  pm2 restart myapp
-else
-  echo "Starting new app with PM2..."
-  pm2 start dist/server.js --name myapp
-fi
-
-pm2 save
